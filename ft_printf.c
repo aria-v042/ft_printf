200a7299d78afbb49041c4a24119edac504b4c5b
aria <redcore_one@proton.me>
2026-05-25 04:35:56 +0100

    add: core loop of ft_printf function

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 03:33:06 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/05/25 03:54:54 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/05/25 04:32:57 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,24 @@
 
 int	ft_printf(const char *format, ...)
 {
+	va_list	args;
+	int		bytes_printed;
 
+	bytes_printed = 0;
+	args = va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			bytes_printed += // TODO
+		}
+		else
+		{
+			ft_putchar_fd(*format, STDOUT_FILENO);
+			bytes_printed++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (bytes_printed);
 }
