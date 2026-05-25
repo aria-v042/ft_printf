5eed96f53d2b03390821a161d52377483ef03f74
aria <redcore_one@proton.me>
2026-05-25 03:46:10 +0100

    header: add header file for ft_printf

---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/25 03:38:26 by frodrig2          #+#    #+#             */
+/*   Updated: 2026/05/25 03:43:51 by frodrig2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include "libft/libft.h"
+
+int	ft_printf(const char *, ...);
+
+#endif
