ff25e11c64e10bdc8ffbf604bda2ecf098aaa5cb
aria <redcore_one@proton.me>
2026-05-29 18:12:24 +0100

    test: add test for pointer specifier case

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/29 17:35:11 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/05/29 17:44:56 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/05/29 18:11:47 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,17 @@ void	test_printf_string(int test_num, const char *s)
 	printf(">> printf() printed %d chars\n\n", bytes_printed);
 }
 
+void	test_printf_pointer(int test_num, void *p)
+{
+	int	bytes_printed;
+
+	printf("-------\ntest %d:\n\n", test_num);
+	bytes_printed = ft_printf("%p\n", p);
+	printf(">> ft_printf() printed %d chars\n", bytes_printed);
+	bytes_printed = printf("%p\n", p);
+	printf(">> printf() printed %d chars\n\n", bytes_printed);
+}
+
 void	test_printf_integer(int test_num, int d)
 {
 	int	bytes_printed;
@@ -51,5 +62,6 @@ int	main(void)
 	test_printf_char(1, 'f');
 	test_printf_string(2, "testing");
 	test_printf_string(3, NULL);
+	test_printf_pointer(4, &test_printf_pointer);
 	return (0);
 }
