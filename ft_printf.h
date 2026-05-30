a790d52148296ee0c2f9232f9c34c4be2c35efca
aria <redcore_one@proton.me>
2026-05-30 01:34:34 +0100

    header: add prototypes of all implemented functions

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 03:38:26 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/05/25 03:54:43 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/05/29 21:49:41 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,20 @@
 # include <stdarg.h>
 # include "libft/libft.h"
 
-int	ft_printf(const char *format, ...);
+/* ==== CORE ==== */
+
+int		ft_printf(const char *format, ...);
+
+/* ==== FORMAT CONVERSION ==== */
+
+int		convert_format(const char *format, va_list *ap_ptr);
+
+int		print_char(va_list *ap_ptr);
+int		print_string(va_list *ap_ptr);
+int		print_pointer(va_list *ap_ptr);
+
+/* ==== UTILS ==== */
+
+void	util_putnbr_base(unsigned long long n, char *base, int *len);
 
 #endif
