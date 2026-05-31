2c9ccf92733d95f1d041666e77c6409e045cb1b5
aria <redcore_one@proton.me>
2026-05-31 05:58:02 +0100

    test: style changes

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: frodrig2 <frodrig2@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/29 17:35:11 by frodrig2          #+#    #+#             */
-/*   Updated: 2026/05/31 05:25:02 by frodrig2         ###   ########.fr       */
+/*   Updated: 2026/05/31 05:56:47 by frodrig2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	test_printf_nospec(int test_num, const char *str)
 {
 	int	bytes_printed;
 
-	printf("-------\ntest %d:\n\n", test_num);
+	printf("-------\ntest %d: string with no specifiers\n\n", test_num);
 	bytes_printed = ft_printf(str);
 	printf(">> ft_printf() printed %d chars\n", bytes_printed);
 	bytes_printed = printf(str);
@@ -28,7 +28,7 @@ void	test_printf_char(int test_num, char c)
 {
 	int	bytes_printed;
 
-	printf("-------\ntest %d:\n\n", test_num);
+	printf("-------\ntest %d: %%c\n\n", test_num);
 	bytes_printed = ft_printf("%c\n", c);
 	printf(">> ft_printf() printed %d chars\n", bytes_printed);
 	bytes_printed = printf("%c\n", c);
@@ -39,7 +39,7 @@ void	test_printf_string(int test_num, const char *s)
 {
 	int	bytes_printed;
 
-	printf("-------\ntest %d:\n\n", test_num);
+	printf("-------\ntest %d: %%s\n\n", test_num);
 	bytes_printed = ft_printf("%s\n", s);
 	printf(">> ft_printf() printed %d chars\n", bytes_printed);
 	bytes_printed = printf("%s\n", s);
@@ -50,7 +50,7 @@ void	test_printf_pointer(int test_num, void *p)
 {
 	int	bytes_printed;
 
-	printf("-------\ntest %d:\n\n", test_num);
+	printf("-------\ntest %d: %%p\n\n", test_num);
 	bytes_printed = ft_printf("%p\n", p);
 	printf(">> ft_printf() printed %d chars\n", bytes_printed);
 	bytes_printed = printf("%p\n", p);
@@ -61,20 +61,26 @@ void	test_printf_integer(int test_num, int intgr)
 {
 	int	bytes_printed;
 
-	printf("-------\ntest %d:\n\n", test_num);
-	bytes_printed = ft_printf("%i\n", intgr);
-	ft_printf(">> ft_printf() printed %d chars\n", bytes_printed);
-	bytes_printed = printf("%i\n", intgr);
+	printf("-------\ntest %d: %%d, %%i\n\n", test_num);
+
+	bytes_printed = ft_printf("%d\n", intgr);
+	printf(">> ft_printf() printed %d chars\n", bytes_printed);
+	bytes_printed = printf("%d\n", intgr);
 	printf(">> printf() printed %d chars\n\n", bytes_printed);
+
+	bytes_printed = ft_printf("%d\n", intgr);
+	printf(">> ft_printf() printed %i chars\n", bytes_printed);
+	bytes_printed = printf("%d\n", intgr);
+	printf(">> printf() printed %i chars\n\n", bytes_printed);
 }
 
 void	test_printf_unsigned(int test_num, int unsgn)
 {
 	int	bytes_printed;
 
-	printf("-------\ntest %d:\n\n", test_num);
+	printf("-------\ntest %d: %%u\n\n", test_num);
 	bytes_printed = ft_printf("%u\n", unsgn);
-	ft_printf(">> ft_printf() printed %d chars\n", bytes_printed);
+	printf(">> ft_printf() printed %d chars\n", bytes_printed);
 	bytes_printed = printf("%u\n", unsgn);
 	printf(">> printf() printed %d chars\n\n", bytes_printed);
 }
@@ -83,13 +89,15 @@ void	test_printf_hexadecimal(int test_num, int hex)
 {
 	int	bytes_printed;
 
-	printf("-------\ntest %d:\n\n", test_num);
+	printf("-------\ntest %d: %%x, %%X\n\n", test_num);
+
 	bytes_printed = ft_printf("%x\n", hex);
-	ft_printf(">> ft_printf() printed %d chars\n", bytes_printed);
+	printf(">> ft_printf() printed %d chars\n", bytes_printed);
 	bytes_printed = printf("%x\n", hex);
 	printf(">> printf() printed %d chars\n\n", bytes_printed);
+
 	bytes_printed = ft_printf("%X\n", hex);
-	ft_printf(">> ft_printf() printed %d chars\n", bytes_printed);
+	printf(">> ft_printf() printed %d chars\n", bytes_printed);
 	bytes_printed = printf("%X\n", hex);
 	printf(">> printf() printed %d chars\n\n", bytes_printed);
 }
